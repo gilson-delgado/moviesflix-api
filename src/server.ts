97062ebc7e2beb7d1a9b0a5e8affc4ce28cb1830
gilson-delgado <gcdelgaa@gmail.com>
2025-03-01 20:27:39 -0300

    configurações iniciais do projeto

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+
+  res.setHeader("Content-type", "text/plain");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("Home page");
+  } else if (req.url === "/about") {
+    res.statusCode = 200;
+    res.end("About page");
+  } else {
+    res.statusCode = 404;
+    res.end("Page not found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Servidor em execução em http://localhost:3000/");
+});
