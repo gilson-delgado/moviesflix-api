2b08585f32973d3ce18b6ecefe4b6db6ac109128
gilson-delgado <gcdelgaa@gmail.com>
2025-03-03 19:45:45 -0300

    excluido comentarios de codigo duplicados

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,3 @@
-// const http = require("http");
-// const server = http.createServer((req, res) => {
-
-//   res.setHeader("Content-type", "text/plain");
-//   if (req.url === "/") {
-//     res.statusCode = 200;
-//     res.end("Teste");
-//   } else if (req.url === "/about") {
-//     res.statusCode = 200;
-//     res.end("Teste page");
-//   } else {
-//     res.statusCode = 404;
-//     res.end("Page not found");
-//   }
-// });
-
-// server.listen(3000, () => {
-//   console.log("Servidor em execução em http://localhost:3000/");
-// });
-
-
-
 import { createServer } from 'http';
 
 const server = createServer((req, res) => {
