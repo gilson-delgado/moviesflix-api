3d51ca493d4f6fe66b4000468af689d9af9f20ac
gilson-delgado <gcdelgaa@gmail.com>
2025-03-03 19:21:27 -0300

    troca de Teste page para Teste abaut

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,35 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
+// const http = require("http");
+// const server = http.createServer((req, res) => {
 
+//   res.setHeader("Content-type", "text/plain");
+//   if (req.url === "/") {
+//     res.statusCode = 200;
+//     res.end("Teste");
+//   } else if (req.url === "/about") {
+//     res.statusCode = 200;
+//     res.end("Teste page");
+//   } else {
+//     res.statusCode = 404;
+//     res.end("Page not found");
+//   }
+// });
+
+// server.listen(3000, () => {
+//   console.log("Servidor em execução em http://localhost:3000/");
+// });
+
+
+
+import { createServer } from 'http';
+
+const server = createServer((req, res) => {
   res.setHeader("Content-type", "text/plain");
   if (req.url === "/") {
     res.statusCode = 200;
     res.end("Teste");
   } else if (req.url === "/about") {
     res.statusCode = 200;
-    res.end("Teste page");
+    res.end("Teste about");
   } else {
     res.statusCode = 404;
     res.end("Page not found");
@@ -16,4 +38,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, () => {
   console.log("Servidor em execução em http://localhost:3000/");
-});
+}); 
