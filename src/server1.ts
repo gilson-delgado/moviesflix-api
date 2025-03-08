849737add207b769d571f58a9e27efc0fda00ae4
gilson-delgado <gcdelgaa@gmail.com>
2025-03-08 09:12:32 -0300

    configurações iniciais do prisma

--- src/server1.ts
+++ src/server1.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/movies", (req,res) => {
-    res.send("listagem de filmes");
+
+
+app.get("/movies", async(req,res) => {
+    const movies = await prisma.movies.findMany();
+    res.json(movies);
 })
 
 app.listen(port, () => {
